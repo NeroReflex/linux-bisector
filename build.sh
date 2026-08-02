77795ba7803936607e578d34e850bb6a53fdfd1a
Denis Benato <benato.denis96@gmail.com>
2026-08-02 21:33:26 +0200

    Modernize config

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ basedir=$(pwd)
 sudo rm -rf src pkg *.tar.zst *.tar.gz
 cd "$basedir/linux"
 git log -1 --pretty=format:"%H%n" | tee -a "$basedir/built_commits.txt"
+echo "\n" | tee -a "$basedir/built_commits.txt"
 git rev-parse --short HEAD > .bisector_commit
 cd "$basedir"
 tar --exclude-vcs -czvf linux.tar.gz ./linux/
